b5059da400d0d4a7ec4269cad39da2cc5ebc6d6f
nikvijay07 <nikvijay07@gmail.com>
2023-07-21 11:18:51 +0200

    Added User Home Screen

---
+++ src/main/java/com/example/traveljournalv2/HomeScreenController.java
@@ -0,0 +1,41 @@
+package com.example.traveljournalv2;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class HomeScreenController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void createJournalEntry(ActionEvent event) {
+
+    }
+
+    @FXML
+    void createTrip(ActionEvent event) {
+
+    }
+
+    @FXML
+    void searchCities(ActionEvent event) {
+
+    }
+
+    @FXML
+    void viewTripEntries(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
--- src/main/java/com/example/traveljournalv2/LoginController.java
+++ src/main/java/com/example/traveljournalv2/LoginController.java
@@ -6,6 +6,7 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -19,33 +20,37 @@ public class LoginController {
     @FXML
     private Scene scene;
     @FXML
-    private Parent root;
-    @FXML
     private URL location;
 
     @FXML
     void loginButton(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(getClass().getResource("UserHomeScreen.fxml"));
-        setScene();
-    }
-
-    @FXML
-    void registerButton(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(getClass().getResource("CreateAccountScreen.fxml"));
-        setScene();
-
-    }
-
-    @FXML
-    void initialize() {
-
-    }
-
-    private void setScene() {
-        stage = (Stage) root.getScene().getWindow();
+        System.out.println("test1");
+        Parent root = FXMLLoader.load(getClass().getResource("UserHomeScreen.fxml"));
+        System.out.println("test2");
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        System.out.println("test3");
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
+
+    @FXML
+    void registerButton(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("CreateAccountScreen.fxml"));
+//        setScene();
+
+    }
+
+//    @FXML
+//    void initialize() {
+//
+//    }
+
+//    private void setScene() {
+//        stage = (Stage) root.getScene().getWindow();
+//        scene = new Scene(root);
+//        stage.setScene(scene);
+//        stage.show();
+//    }
 
 }
