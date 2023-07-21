440ec5e36ab6be130cf8a634c0558207a28defcc
gmirzazadeh <gmirzazadeh@gatech.edu>
2023-07-21 11:14:45 +0200

    added connection

---
+++ src/main/java/com/example/traveljournalv2/DatabaseConnection.java
@@ -0,0 +1,26 @@
+package com.example.traveljournalv2;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DatabaseConnection {
+    public Connection databaseLink;
+
+    public Connection getConnection() {
+        String databaseName = "phaseii";
+        String databaseUser = "root";
+        String databasePassword = "johannes";
+
+        String url = "jdbc:mysql://localhost/" + databaseName;
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            databaseLink = DriverManager.getConnection(url, databaseUser, databasePassword);
+
+        } catch(Exception e) {
+
+        }
+        return databaseLink;
+    }
+
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.example.traveljournalv2 {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
+    requires java.sql;
 
 
     opens com.example.traveljournalv2 to javafx.fxml;
