a160c68ec951ea06f8ac49cc8e33bd5b45e27a20
robster165 <139757760+robster165@users.noreply.github.com>
2023-07-23 23:26:35 +0200

    added CreateAccountScreenController.java and fixed CreateAccountScreen.fxml

---
+++ src/main/java/com/example/traveljournalv2/CreateAccountScreenController.java
@@ -0,0 +1,68 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class CreateAccountScreenController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button create;
+
+    @FXML
+    private TextField email;
+
+    @FXML
+    private TextField fname;
+
+    @FXML
+    private TextField lname;
+
+    @FXML
+    private Button no;
+
+    @FXML
+    private TextField pass;
+
+    @FXML
+    private TextField uname;
+
+    @FXML
+    private Button yes;
+
+    @FXML
+    void adminButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void backButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void createAccount(ActionEvent event) {
+
+    }
+
+//    @FXML
+//    void initialize() {
+//        assert create != null : "fx:id=\"create\" was not injected: check your FXML file 'CreateAccountScreen.fxml'.";
+//        assert email != null : "fx:id=\"email\" was not injected: check your FXML file 'CreateAccountScreen.fxml'.";
+//        assert fname != null : "fx:id=\"fname\" was not injected: check your FXML file 'CreateAccountScreen.fxml'.";
+//        assert lname != null : "fx:id=\"lname\" was not injected: check your FXML file 'CreateAccountScreen.fxml'.";
+//        assert no != null : "fx:id=\"no\" was not injected: check your FXML file 'CreateAccountScreen.fxml'.";
+//        assert pass != null : "fx:id=\"pass\" was not injected: check your FXML file 'CreateAccountScreen.fxml'.";
+//        assert uname != null : "fx:id=\"uname\" was not injected: check your FXML file 'CreateAccountScreen.fxml'.";
+//        assert yes != null : "fx:id=\"yes\" was not injected: check your FXML file 'CreateAccountScreen.fxml'.";
+//
+//    }
+//
+//}
