85081a1b0f6e049abac6780f914099198fd919ca
nikvijay07 <nikvijay07@gmail.com>
2023-07-24 11:25:25 +0200

    push

---
+++ src/main/java/com/example/traveljournalv2/ReviewFlaggedEntryController.java
@@ -0,0 +1,55 @@
+package com.example.traveljournalv2;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class ReviewFlaggedEntryController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField city;
+
+    @FXML
+    private TextField date;
+
+    @FXML
+    private TextArea note;
+
+    @FXML
+    private TextField rating;
+
+    @FXML
+    void backButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void banUser(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clearFlag(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deleteEntry(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
