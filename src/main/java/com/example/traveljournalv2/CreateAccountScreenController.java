242df5375caa063482149075c6c4ffac1d0e8541
robster165 <139757760+robster165@users.noreply.github.com>
2023-07-24 10:25:32 +0200

    added CreateAccountScreenController.java and fixed CreateAccountScreen.fxml

--- src/main/java/com/example/traveljournalv2/CreateAccountScreenController.java
+++ src/main/java/com/example/traveljournalv2/CreateAccountScreenController.java
@@ -11,6 +11,12 @@ import javafx.scene.control.TextField;
 public class CreateAccountScreenController {
 
     @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
     private CheckBox admin;
 
     @FXML
@@ -38,6 +44,18 @@ public class CreateAccountScreenController {
 
     @FXML
     void createAccount(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert admin != null : "fx:id=\"admin\" was not injected: check your FXML file 'CreateAccountScreen.fxml'.";
+        assert create != null : "fx:id=\"create\" was not injected: check your FXML file 'CreateAccountScreen.fxml'.";
+        assert email != null : "fx:id=\"email\" was not injected: check your FXML file 'CreateAccountScreen.fxml'.";
+        assert fname != null : "fx:id=\"fname\" was not injected: check your FXML file 'CreateAccountScreen.fxml'.";
+        assert lname != null : "fx:id=\"lname\" was not injected: check your FXML file 'CreateAccountScreen.fxml'.";
+        assert pass != null : "fx:id=\"pass\" was not injected: check your FXML file 'CreateAccountScreen.fxml'.";
+        assert uname != null : "fx:id=\"uname\" was not injected: check your FXML file 'CreateAccountScreen.fxml'.";
 
     }
 
