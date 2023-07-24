544894a1db1f49c658d4561488f6fa21c2723cb9
Johannes <johannesq23@gmail.com>
2023-07-25 00:42:09 +0200

    update create journal entry

--- src/main/java/com/example/traveljournalv2/CreateCityJournalEntryController.java
+++ src/main/java/com/example/traveljournalv2/CreateCityJournalEntryController.java
@@ -58,77 +58,52 @@ public class CreateCityJournalEntryController {
 
     @FXML
     void saveButton(ActionEvent event) throws SQLException {
-<<<<<<< Updated upstream
-
 
         try {
-        DatabaseConnection ConnectNow = new DatabaseConnection();
-        Connection connectDB = ConnectNow.getConnection();
 
-=======
-        DatabaseConnection ConnectNow = new DatabaseConnection();
-        Connection connectDB = ConnectNow.getConnection();
+            DatabaseConnection ConnectNow = new DatabaseConnection();
+            Connection connectDB = ConnectNow.getConnection();
+            String locationID = null;
+            String privacy = (privacyLevel.isSelected() ? "Public" : "Private");
 
-        String locationID = null;
-        String privacy = (privacyLevel.isSelected() ? "Public" : "Private");
+            String cityName = city.getText();
+            String cityQuery = "SELECT Location_ID FROM City WHERE Cname = '" + cityName + "'";
 
-        String cityName = city.getText();
-        String cityQuery = "SELECT Location_ID FROM City WHERE Cname = '"  + cityName + "'";
+            Statement statement = connectDB.createStatement();
+            ResultSet queryOutput = statement.executeQuery(cityQuery);
 
-        Statement statement = connectDB.createStatement();
-        ResultSet queryOutput = statement.executeQuery(cityQuery);
-
-        if(queryOutput.next()) {
-            locationID = queryOutput.getString("Location_ID");
-        }
->>>>>>> Stashed changes
+            if (queryOutput.next()) {
+                locationID = queryOutput.getString("Location_ID");
+            }
 
 
-        String connectQuery = "INSERT INTO Journal_Entry (Note, Rating, Date, Privacy_Level, Author_Email, Location_ID) VALUES (?,?,?,?,?,?)";
-        PreparedStatement preparedStatement = connectDB.prepareStatement(connectQuery);
 
-<<<<<<< Updated upstream
+            String connectQuery = "INSERT INTO Journal_Entry (Note, Rating, Date, Privacy_Level, Author_Email, Location_ID) VALUES (?,?,?,?,?,?)";
+            PreparedStatement preparedStatement = connectDB.prepareStatement(connectQuery);
+
+            preparedStatement.setString(1, note.getText());
+            preparedStatement.setInt(2, Integer.valueOf(rating.getText()));
+            preparedStatement.setString(3, cityDate.getValue().toString());
+            preparedStatement.setString(4, privacy);
+            preparedStatement.setString(5, User.email);
+            preparedStatement.setString(6, locationID);
+
+            System.out.println(preparedStatement.toString());
+
+            int out = preparedStatement.executeUpdate();
 
 
-        preparedStatement.setString(1,note.getText());
-        preparedStatement.setString(2,rating.getText().toString());
-        preparedStatement.setString(3,cityDate.getValue().toString());
-        preparedStatement.setString(4,User.email);
-        //we need to figure out how to do the location id query and add that as the last parameter
-
-
-        int rowsAffected = preparedStatement.executeUpdate();
-        System.out.println("Rows affected: " + rowsAffected);
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
-=======
-        User.email = "test@gmail.com";
-
-        preparedStatement.setString(1,note.getText());
-        preparedStatement.setString(2,rating.toString());
-        preparedStatement.setString(3,cityDate.getValue().toString());
-        preparedStatement.setString(4,privacy);
-        preparedStatement.setString(5,User.email);
-        preparedStatement.setString(6,locationID);
-
-        System.out.println(preparedStatement.toString());
-
-        try {
-            //Statement statement = connectDB.createStatement();
-            int out = preparedStatement.executeUpdate();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
 
 
 
-
->>>>>>> Stashed changes
 
     }
+
+
 
     @FXML
     void initialize() {
