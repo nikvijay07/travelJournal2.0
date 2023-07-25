2e5176062a9041d1c164f9203d463c0382a0d772
robster165 <139757760+robster165@users.noreply.github.com>
2023-07-25 21:01:14 +0200

    added back button on userSettings

--- src/main/java/com/example/traveljournalv2/UserSettingsController.java
+++ src/main/java/com/example/traveljournalv2/UserSettingsController.java
@@ -1,12 +1,18 @@
 package com.example.traveljournalv2;
 
+import java.io.IOException;
 import java.net.URL;
 import java.sql.*;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleButton;
+import javafx.stage.Stage;
 
 public class UserSettingsController {
 
@@ -34,6 +40,18 @@ public class UserSettingsController {
     @FXML
     private ToggleButton publicPrivacy;
 
+    @FXML
+    private Stage stage;
+    @FXML
+    private Scene scene;
+    @FXML
+    void backButton(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("UserHomeScreen.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
     @FXML
     void deleteAccountButton(ActionEvent event) {
 
