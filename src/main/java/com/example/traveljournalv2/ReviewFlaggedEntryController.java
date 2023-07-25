832705059590e314c3332dbbde048e36ed604101
robster165 <139757760+robster165@users.noreply.github.com>
2023-07-26 00:24:24 +0200

    Update ReviewFlaggedEntryController.java

--- src/main/java/com/example/traveljournalv2/ReviewFlaggedEntryController.java
+++ src/main/java/com/example/traveljournalv2/ReviewFlaggedEntryController.java
@@ -1,11 +1,17 @@
 package com.example.traveljournalv2;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class ReviewFlaggedEntryController {
 
@@ -26,10 +32,18 @@ public class ReviewFlaggedEntryController {
 
     @FXML
     private TextField rating;
+    @FXML
+    private Stage stage;
+    @FXML
+    private Scene scene;
 
     @FXML
-    void backButton(ActionEvent event) {
-
+    void backButton(ActionEvent event) throws IOException {
+            Parent root = FXMLLoader.load(getClass().getResource("AdminFlagsHomePage.fxml"));
+            stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
     }
 
     @FXML
